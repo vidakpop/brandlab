855b7217db17697a2358578f1de159fd9e80b87a
vidakpop <daviskpop@gmail.com>
2024-11-15 16:17:13 +0300

    "Added new elements and styles to Home component"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,58 @@ const Home = () => {
     <HomeContainer>
         <Hero>
             <h1>Welcome to Imelda's Brand Lab</h1>
+            <p>Empowering Brands,Transforming Identities</p>
+            <HeroButton>Learn More</HeroButton>
         </Hero>
+        <Section>
+            <h2>About The Merger Program</h2>
+            <p>Join a community focused on brand growth and digital transformation</p>
+            <SectionButton>
+                Join Now
+            </SectionButton>
+        </Section>
     </HomeContainer>
   )
 }
 
 export default Home
+
+const HomeContainer = styled.div`
+  text-align: center;
+`;
+
+const Hero = styled.div`
+  background-color: #f39c12;
+  padding: 100px 0;
+  color: #fff;
+`;
+const HeroButton = styled.button`
+  background-color: #f39c12;
+  border: none;
+  padding: 10px 20px;
+  color: #fff;
+  cursor:pointer;
+  font-size: 1rem;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: #e67e22;
+  }
+`;
+const Section = styled.div`
+  padding: 1rem;
+  margin: 2rem 0;
+`;
+const SectionButton = styled.button`
+  background-color: #f39c12;
+  border: none;
+  padding: 10px 20px;
+  color: #fff;
+  cursor:pointer;
+  font-size: 1rem;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: #e67e22;
+  }
+`;  
