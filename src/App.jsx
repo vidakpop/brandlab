7592bfd3f5799280f28f69ffc2c0681a2f185adc
vidakpop <daviskpop@gmail.com>
2024-11-15 15:51:05 +0300

    "Added Routes and Route components from react-router-dom, and a Home page component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home' 
+
 
 const App = () => {
   return (
     <Router>
     
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+      </Routes>
 
 
     </Router>
