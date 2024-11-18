214e59ac46d7c7a742f38f051c47735653e78c29
vidakpop <daviskpop@gmail.com>
2024-11-18 23:30:05 +0300

    "Added import statement for About page component"

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home' 
+import About from './pages/About'
 
 
 const App = () => {
