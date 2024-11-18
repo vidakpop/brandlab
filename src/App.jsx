a49ee48253584c08101d7f286420225fb59cafa9
vidakpop <daviskpop@gmail.com>
2024-11-18 23:27:44 +0300

    "Added About route to App.jsx and updated About.jsx with new content and container"

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<h1>About</h1>} />        
 
       </Routes>
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <AboutContainer>
+        <h2>About Me</h2>
+        <p>Hi, I'm Imelda, a brand strategist and designer. I help brands grow and transform their identities. I have over 10 years of experience in brand strategy, design, and digital marketing. I have worked with brands from various industries, including tech, fashion, and finance. I am passionate about helping brands reach their full potential and stand out in the market.</p>   
+    </AboutContainer>
   )
 }
 
