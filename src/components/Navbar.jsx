2dd6efed39b1a0206325ba6ad60573be5bbc061f
vidakpop <daviskpop@gmail.com>
2024-12-29 20:13:45 +0300

    Enhance Navbar component with branding and navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,17 @@ import { Link } from 'react-scroll'
 const Navbar = () => {
   return (
     <nav className='fixed top-0 left-0 w-full bg-black bg-opacity-50 z-50'>
-      
+      <div className='max-w-7xl mx-auto px-6 py-6 flex justify-between items-center'>
+        <h1 className='text-white text-2xl font-bold'>
+          IMELDA BRAND LAB
+        </h1>
+        <li>
+          <Link to='hero' smooth duration={500} className='hover:underline'>
+              Home
+          </Link>
+        </li>
+
+      </div>
 
     </nav>
   )
