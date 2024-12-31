5a0473785c754f72028ec201ab90cceba79b2f7e
vidakpop <daviskpop@gmail.com>
2024-12-31 20:22:42 +0300

    Enhance AboutSection with animated heading and improved styling

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import { motion } from 'framer-motion'
 const AboutSection = () => {
   return (
-    <div>AboutSection</div>
+    <div
+        id='about'
+        className='nax-w-7xl mx-auto px-6 py-20 text-center bg-gray-100'
+    >
+        <motion.h2
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:1}}
+            className='text-4xl font-bold mb-6'
+        >
+            About Imelda
+        </motion.h2>
+
+    </div>
   )
 }
 
