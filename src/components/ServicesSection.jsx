a86e9feff1c7832b11f99a1616c76da24f2e5082
vidakpop <daviskpop@gmail.com>
2025-01-01 12:57:51 +0300

    Add service items with icons to ServicesSection component

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import { FaBriefcase, FaLaptop, FaPencilAlt, FaChartLine, FaCogs } from "react-icons/fa";
+
+const services=[
+    { icon: <FaBriefcase />, title: "Resume Writing" },
+    { icon: <FaLaptop />, title: "Digital Strategy" },
+    { icon: <FaPencilAlt />, title: "Content Creation" },
+    { icon: <FaChartLine />, title: "Social Media Strategy" },
+    { icon: <FaCogs />, title: "Brand Consulting" },
+    { icon: <FaLaptop />, title: "LinkedIn Optimization" },
+]
 const ServicesSection = () => {
   return (
     <div>ServicesSection</div>
