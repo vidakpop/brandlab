84c3602d267b9419796eb52620f0b659d18e6422
vidakpop <daviskpop@gmail.com>
2025-01-01 10:32:24 +0300

    Add ProfileSection component for user profile display

---
+++ src/components/ProfileSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const ProfileSection = () => {
+  return (
+    <div>ProfileSection</div>
+  )
+}
+
+export default ProfileSection
