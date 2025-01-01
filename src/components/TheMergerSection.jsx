d3651ca7acb270b42eb3db5409a3ff1a66e47b08
vidakpop <daviskpop@gmail.com>
2025-01-01 11:52:52 +0300

    Add cohort data structure to TheMergerSection component

--- src/components/TheMergerSection.jsx
+++ src/components/TheMergerSection.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 const TheMergerSection = () => {
+    const cohorts = [
+        {
+            title: "April Cohort",
+            description: "Community Approach, LinkedIn Posting Challenge, Mentorship, Review",
+          },
+          {
+            title: "August Cohort",
+            description: "Community Approach, LinkedIn Posting Challenge, Mentorship, Review",
+          },
+          {
+            title: "December Cohort",
+            description: "Community Approach, LinkedIn Posting Challenge, Mentorship, Review",
+          },
+    ]
   return  (
     <div>TheMergerSection</div>
   )
