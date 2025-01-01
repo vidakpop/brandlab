3a9214b7bd8d18458e972148156230082bd23f40
vidakpop <daviskpop@gmail.com>
2025-01-01 12:02:28 +0300

    Add cohort title and description to TheMergerSection component

--- src/components/TheMergerSection.jsx
+++ src/components/TheMergerSection.jsx
@@ -28,9 +28,10 @@ const TheMergerSection = () => {
                     className='bg-white p-6 rounded-lg shadow-lg'
                     whileHover={{scale:1.05}}
                 >
-
+                    <h3 className='text-2xl font-bold mb-4'>{cohort.title}</h3>
+                    <p className='text-gray-700'>{cohort.description}</p>
                 </motion.div>
-            )}
+            ))}
 
         </div>
 
