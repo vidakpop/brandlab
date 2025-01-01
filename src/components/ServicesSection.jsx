999035b473153e868ab50aef3dcf8cecee139695
vidakpop <daviskpop@gmail.com>
2025-01-01 12:55:53 +0300

    Add icons to ServicesSection component

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { FaBriefcase, FaLaptop, FaPencilAlt, FaChartLine, FaCogs } from "react-icons/fa";
 const ServicesSection = () => {
   return (
     <div>ServicesSection</div>
