3a36ee194d99d31c40b47234db7cd2cd17eb7ea7
vidakpop <daviskpop@gmail.com>
2025-01-01 11:54:28 +0300

    Refactor TheMergerSection layout with new container styles

--- src/components/TheMergerSection.jsx
+++ src/components/TheMergerSection.jsx
@@ -17,7 +17,10 @@ const TheMergerSection = () => {
           },
     ]
   return  (
-    <div>TheMergerSection</div>
+    <div id='merger' className='max-w-7xl mx-auto px-6 py-20'>
+        
+
+    </div>
   )
 }
  
