67f95bda20cbbd03a83c557730f354a78c0972f7
vidakpop <daviskpop@gmail.com>
2025-01-01 13:05:53 +0300

    Enhance heading style in ServicesSection component for better visibility

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -15,7 +15,7 @@ const services = [
 const ServicesSection = () => {
   return (
     <div id='services' className='max-w-7xl mx-auto px-6 py-20 bg-gray-100'>
-        <h2>Our Services</h2>
+        <h2 className='text-4xl font-bold text-center mb-10'>Our Services</h2>
 
     </div>
   )
