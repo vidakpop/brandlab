ce1898df5f28bb8c4e21b8b859daa8c11ec087be
vidakpop <daviskpop@gmail.com>
2025-01-02 15:39:25 +0300

    Add id attribute to contact button in ProfileSection for improved accessibility

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -93,6 +93,7 @@ const ProfileSection = () => {
         >
           <motion.button
             whileHover={{ scale: 1.1 }}
+            id='contact'
             className="px-8 py-3 bg-purple-500 text-white font-bold rounded-full shadow-lg hover:bg-purple-600"
           >
             Connect with Imelda
