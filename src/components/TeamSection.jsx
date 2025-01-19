cf59b2b60f289c71c7cc46d14eb827c3cc6da3a6
vidakpop <daviskpop@gmail.com>
2025-01-19 15:41:58 +0300

    Add team members data to TeamSection component

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
-
+const TeamSection = () => {
+    const teamMenbers=[
+        {
+            name:'Imelda Mugambi',
+            position:'Founder & CEO',
+            linkedin:'https://www.linkedin.com/in/imeldamugambi/',
+            photo:"",
+        },
+    ]
   return (
     <div>TeamSection</div>
   )
